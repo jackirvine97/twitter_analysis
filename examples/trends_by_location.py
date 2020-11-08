0a74d01e03c04678cfd740c341c547bb636a5906
jackirvine97 <wji20@cam.ac.uk>
2020-11-08 22:03:38 +0000

    Add trends example

---
+++ examples/trends_by_location.py
@@ -0,0 +1,15 @@
+"""This searches for trending topics in a specified location."""
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Create API object
+api = tweepy.API(auth)
+
+# id – The Yahoo! Where On Earth ID of the location to return trending
+loc_id = 1
+trends_result = api.trends_place(loc_id)
+for trend in trends_result[0]["trends"]:
+    print(trend["name"])
