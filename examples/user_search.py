d5a73f7134f1a4b39a1a3d7130ebbe533c427654
jackirvine97 <wji20@cam.ac.uk>
2020-11-08 18:21:45 +0000

    Add user search script

---
+++ examples/user_search.py
@@ -0,0 +1,20 @@
+"""This script looks up a specified user."""
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Create API object
+api = tweepy.API(auth)
+
+user = api.get_user("BBCJonSopel")
+
+print("User details:")
+print(user.name)
+print(user.description)
+print(user.location)
+
+print("Last 20 Followers:")
+for follower in user.followers():
+    print(follower.name)
