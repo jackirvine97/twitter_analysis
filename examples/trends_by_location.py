4fcfdd9c1de1e04b7d546ccf72a4f5cc1998a79a
jackirvine97 <wji20@cam.ac.uk>
2020-11-11 09:33:25 +0000

    Set location to London

--- examples/trends_by_location.py
+++ examples/trends_by_location.py
@@ -9,7 +9,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # id – The Yahoo! Where On Earth ID of the location to return trending
-loc_id = 1
+loc_id = 44418  # London
 trends_result = api.trends_place(loc_id)
 for trend in trends_result[0]["trends"]:
     print(trend["name"])
