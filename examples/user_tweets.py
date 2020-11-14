1bd7a47c013b67f33e72d513858fccc68e39ac08
jackirvine97 <wji20@cam.ac.uk>
2020-11-14 15:07:50 +0000

    Standardise authetication

--- examples/user_tweets.py
+++ examples/user_tweets.py
@@ -1,5 +1,6 @@
 import tweepy
 
+# Authenticate to Twitter and instantiate API.
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
