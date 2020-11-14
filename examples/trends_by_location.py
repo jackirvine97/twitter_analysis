5a0e68cb93b19fb765212261115b11e4e8447b1b
jackirvine97 <wji20@cam.ac.uk>
2020-11-14 15:08:29 +0000

    Standardise authetication

--- examples/trends_by_location.py
+++ examples/trends_by_location.py
@@ -1,11 +1,9 @@
 """This searches for trending topics in a specified location."""
 import tweepy
 
-# Authenticate to Twitter
+# Authenticate to Twitter and instantiate API.
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-# Create API object
 api = tweepy.API(auth)
 
 # id – The Yahoo! Where On Earth ID of the location to return trending
