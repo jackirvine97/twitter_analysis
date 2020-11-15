02506fc4b11a7a501177ccb942aa2ea6dab53436
jackirvine97 <wji20@cam.ac.uk>
2020-11-15 19:17:51 +0000

    Add star shaped wordcloud

--- examples/search_wordcloud.py
+++ examples/search_wordcloud.py
@@ -1,5 +1,10 @@
+"""This script generates a wordcloud in the shape of a Pret a Manger star."""
+
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 import re
+import time
 import tweepy
 from wordcloud import STOPWORDS, WordCloud
 
@@ -8,9 +13,10 @@ auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-search_term = "pret"
-number_of_tweets = 700
+search_term = ["pret a manger"]
+number_of_tweets = 2000
 
+# Collect tweets.
 tweets = tweepy.Cursor(api.search, q=search_term, lang="en").items(number_of_tweets)
 tweets_text = [tweet.text for tweet in tweets]
 tweet_count = len(tweets_text)
@@ -25,11 +31,21 @@ no_special_characters = re.sub('[^A-Za-z ]+', '', no_unicode)
 words = no_special_characters.split(" ")
 words = [w for w in words if len(w) > 2]
 words = [w.lower() for w in words]
-STOPWORDS.update(["pret", "manger", "pret a manger", "retweet"])
+STOPWORDS.update(["pret", "manger", "pret a manger", "retweet", "pretamanger",
+                  "dr", "janaway", "dr janaway", "drjanaway", "morei", "sissy",
+                  "chefsay", "ferdinand"])
 words = [w for w in words if w not in STOPWORDS]
 filtered_string = ','.join(words)
 
-wordcloud = WordCloud(background_color="white", width=1600, height=800).generate(filtered_string)
+mask = np.array(Image.open('star.png'))
+
+wordcloud = WordCloud(
+    background_color="white",
+    width=1600, height=800,
+    max_words=200,
+    mask=mask,
+)
+wordcloud.generate(filtered_string)
 
 print(f"Number of tweets processed: {tweet_count}")
 
