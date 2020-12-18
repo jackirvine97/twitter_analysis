268d4403b20db77fb1bc74bcfff460d16b294929
jackirvine97 <wji20@cam.ac.uk>
2020-12-18 18:02:18 +0000

    Remove accidental infinite loop

--- examples/search.py
+++ examples/search.py
@@ -2,6 +2,12 @@
 import tweepy
 from utils import search_past_7_days
 
+# Variables that contains the user credentials to access Twitter API
+API_KEY = "XLr54F1FRNE7z5vuCgQDGhREq"
+API_KEY_SECRET = "55jFS2OhBPgrdr343VjmpAfgz1uf3FzA6GQ1OqYeip93OGHsS0"
+ACCESS_TOKEN = "1848494462-VngJMNvp0aYHZRsJTawjfAoLlkxhZWGS25IZNOU"
+ACCESS_TOKEN_SECRET = "GONapY2u3aOALC35KRXOcm0y7uz4eODwAq8ZVRSAGsuFy"
+
 # Authenticate to Twitter and instantiate API.
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -14,8 +20,8 @@ api = tweepy.API(auth)
 
 search_term = "golf"
 tweets = search_past_7_days(search_term, api, max_tweets=50)
+print('hi')
 for tweet in tweets:
-    tweets.append(tweet)
     print(tweet.user.name)
     print(tweet.text)
     print("************************************")
