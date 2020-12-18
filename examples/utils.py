f2b324e5196524badf1ebc86f6d164d798dc1af6
jackirvine97 <wji20@cam.ac.uk>
2020-12-18 20:29:34 +0000

    Add language option to sear ch utility

--- examples/utils.py
+++ examples/utils.py
@@ -1,9 +1,10 @@
 """Temporary collection of utility functions.
 TODO - determine better utility strategy to avoid import errors."""
+import time
 import tweepy
 
 
-def search_past_7_days(search_term, api, *, max_tweets=100):
+def search_past_7_days(search_term, api, *, max_tweets=100, language="en"):
     """Returns specified number of tweets within the last 7 days.
 
     Parameters
@@ -12,6 +13,8 @@ def search_past_7_days(search_term, api, *, max_tweets=100):
         Search query.
     max_tweets: :obj:`int`
         Maximum number of tweets to be scraped. Default is 100.
+    search_term: :obj:`str`
+        Language of tweets.
 
     Returns
     -------
@@ -24,6 +27,7 @@ def search_past_7_days(search_term, api, *, max_tweets=100):
     cursor = tweepy.Cursor(
         api.search,
         q=search_term,
+        language=language,
         include_entities=True).items(max_tweets)
 
     # Gather the date, pausing 15 minutes any time the request limit is hit.
