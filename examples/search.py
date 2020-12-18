cff360fbc0b9604573a37e6a53a15007d0ffbdd5
jackirvine97 <wji20@cam.ac.uk>
2020-12-18 17:54:57 +0000

    Refactor search script to use search util

--- examples/search.py
+++ examples/search.py
@@ -1,5 +1,12 @@
 """This searches for a specified term."""
 import tweepy
+from utils import search_past_7_days
+
+# Variables that contains the user credentials to access Twitter API
+API_KEY = "QqGGXnpQPvifxKoNQgvNmOy7c"
+API_KEY_SECRET = "AJ54J47YPdbhvEdyZprFqN4bmyblNGFeazWDfPWOauY6diVIek"
+ACCESS_TOKEN = "1848494462-sRVdZy86BLjY8IHOqyubfEB8W8ralXQLtupgHlE"
+ACCESS_TOKEN_SECRET = "RHvLeHtuc9hafG08Mq3fNVAIQ9oPPUqLnnU6P7kcxCLNG"
 
 # Authenticate to Twitter and instantiate API.
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
@@ -10,9 +17,10 @@ api = tweepy.API(auth)
 # 100 tweets per request. If a premium account is created the `search_full_archive`
 # method can be used.
 # See: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/overview
+
 search_term = "golf"
-tweets = []
-for tweet in api.search(q=search_term, lang="en", rpp=10, count=100):
+tweets = search_past_7_days(search_term, api, max_tweets=50)
+for tweet in tweets:
     tweets.append(tweet)
     print(tweet.user.name)
     print(tweet.text)
