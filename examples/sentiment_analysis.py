d6d4ee35c93d84909dd21b07ecb84f4dadc1335e
jackirvine97 <wji20@cam.ac.uk>
2021-01-24 16:58:48 +0000

    Refactor to comply with data model

--- examples/sentiment_analysis.py
+++ examples/sentiment_analysis.py
@@ -1,65 +1,49 @@
 """This plot runs a sentiment analysis on two inputs and plots results,
 detailing polarity and subjectivity."""
 import matplotlib.pyplot as plt
+import pandas as pd
 from textblob import TextBlob
 import tweepy
+from utils import clean, open_json_as_dataframe
 
-# Authenticate to Twitter and instantiate API.
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# Import standard df.
+df_1, meta = open_json_as_dataframe("../data/ice_ban-23-Jan-2021.json")
+df_2, meta = open_json_as_dataframe("../data/ev-24-Jan-2021.json")
+df_3, meta = open_json_as_dataframe("../data/electric_car_uk-24-Jan-2021.json")
+df_4, meta = open_json_as_dataframe("../data/electric_vehicle_uk-23-Jan-2021.json")
+df = pd.concat([df_1, df_2, df_3, df_4])
 
-happy_tweets = api.search(
-    q="happy",
-    lang="en",
-    rpp=10,
-    count=200
-)
-brexit_tweets = api.search(
-    q="brexit",
-    lang="en",
-    rpp=10,
-    count=200
-)
+favourites = df.favorite_count.to_list()
+retweets = df.retweet_count.to_list()
 
-happy_polarity = [TextBlob(tweet.text).sentiment.polarity for tweet in happy_tweets]
-happy_subjectivity = [TextBlob(tweet.text).sentiment.subjectivity for tweet in happy_tweets]
-brexit_polarity = [TextBlob(tweet.text).sentiment.polarity for tweet in brexit_tweets]
-brexit_subjectivity = [TextBlob(tweet.text).sentiment.subjectivity for tweet in brexit_tweets]
+tweet_list = df.text.to_list()
+tweet_list = clean(tweet_list)
 
-neutral_threshold = 0.05
-happy_number_positive = sum(pol > neutral_threshold for pol in happy_polarity)
-happy_number_negative = sum(pol < -neutral_threshold for pol in happy_polarity)
-happy_number_neutral = len(happy_polarity) - happy_number_positive - happy_number_negative
+tweet_polarity = [TextBlob(tweet).polarity for tweet in tweet_list]
+tweet_subjectivity = [TextBlob(tweet).sentiment.subjectivity for tweet in tweet_list]
+df["polarity"] = tweet_polarity
+df["subjectivity"] = tweet_subjectivity
 
-brexit_number_positive = sum(pol > neutral_threshold for pol in brexit_polarity)
-brexit_number_negative = sum(pol < neutral_threshold for pol in brexit_polarity)
-brexit_number_neutral = len(brexit_polarity) - brexit_number_positive - brexit_number_negative
+neutral_threshold = 0
+tweet_num_positive = sum(pol > neutral_threshold for pol in tweet_polarity)
+tweet_num_negative = sum(pol < -neutral_threshold for pol in tweet_polarity)
+tweet_num_neutral = len(tweet_polarity) - tweet_num_positive - tweet_num_negative
 
-# TODO: determine better way to visualise results.
-plt.scatter(happy_polarity, happy_subjectivity, label="Happy")
-plt.scatter(brexit_polarity, brexit_subjectivity, label="Brexit")
-plt.xlabel("Polarity")
-plt.ylabel("Subjectivity")
+cm = plt.cm.get_cmap('GnBu')
+sc = plt.scatter(tweet_polarity, tweet_subjectivity, s=favourites, alpha=0.65,
+                 c=retweets, cmap=cm, vmin=-18, vmax=50)
+plt.xlabel("Polarity (Positive v Negative)")
+plt.ylabel("Subjectivity (How Emotive?)")
 plt.xlim(-1.05, 1.05)
 plt.ylim(-0.05, 1.05)
-plt.legend()
 plt.show()
 
+
 labels = ['Positive', 'Neutral', 'Negative']
-sizes = [happy_number_positive, happy_number_neutral, happy_number_negative]
+sizes = [tweet_num_positive, tweet_num_neutral, tweet_num_negative]
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         startangle=90)
-ax1.set_title("Happy")
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-labels = ['Positive', 'Neutral', 'Negative']
-sizes = [brexit_number_positive, brexit_number_neutral, brexit_number_negative]
-fig2, ax2 = plt.subplots()
-ax2.pie(sizes, labels=labels, autopct='%1.1f%%',
-        startangle=90)
-ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-ax2.set_title("Brexit")
 
 plt.show()
