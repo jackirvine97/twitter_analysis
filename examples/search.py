83d526fe00822279924129ad4444cb3158c6ac52
jackirvine97 <wji20@cam.ac.uk>
2021-02-28 15:32:27 +0000

    Remove API keys

--- examples/search.py
+++ examples/search.py
@@ -9,13 +9,19 @@ See: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/overview
 import tweepy
 from utils import (open_json_as_dataframe, save_tweets_as_json, search_past_7_days)
 
+
 # Authenticate to Twitter and instantiate API.
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-search_term = "physics AND maths"
-tweets = search_past_7_days(search_term, api, max_tweets=10)
-
-save_tweets_as_json(tweets, filename="../data/subjects.json", search_term=search_term)
+# Search term logic can be confirmed by doing an advanced search on Twitter
+# web browser.
+search_term = "Patrick Reed"
+tweets = search_past_7_days(search_term, api, max_tweets=2000)
 print(f"Number of tweets pulled: {len(tweets)}")
+
+save_tweets_as_json(tweets, filename="../data/Farmers_Insurance.json", search_term=search_term)
+
+# df, meta = open_json_as_dataframe("../data/Farmers_Insurance-31-Jan-2021.json")
+# df.to_excel("TESTTTTTTTTT.xlsx")
