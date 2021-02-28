bc0af24d9ad2f47ff137531386883efd57eba98f
jackirvine97 <wji20@cam.ac.uk>
2021-02-28 15:30:37 +0000

    Add script to porduce excel

---
+++ examples/read_json_to_excel.py
@@ -0,0 +1,10 @@
+"""Reads saved tweet JSON to dataframe."""
+import pandas as pd
+from utils import open_json_as_dataframe
+
+output_filename = "../excel_placeholder/ICE_Ban_Tweets.xlsx"
+
+dfs_1 = [open_json_as_dataframe(f"../data/ICE_ban_November_2020_{index}-31-Jan-2021.json")[0] for index in range(1, 5)]
+dfs_2 = [open_json_as_dataframe(f"../data/ICE_ban_November_2020_{index}-01-Feb-2021.json")[0] for index in range(5, 10)]
+df = pd.concat(dfs_1 + dfs_2)
+df.to_excel(output_filename)
